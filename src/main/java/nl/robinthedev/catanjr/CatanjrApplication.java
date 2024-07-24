063b2f6bed2b60b16763f245add70a929ff23881
Robin van den Bogaard <rjd.vandenbogaard@gmail.com>
2024-07-24 22:27:18 +0200

    Initial commit

---
+++ src/main/java/nl/robinthedev/catanjr/CatanjrApplication.java
@@ -0,0 +1,13 @@
+package nl.robinthedev.catanjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatanjrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatanjrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/nl/robinthedev/catanjr/CatanjrApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.robinthedev.catanjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanjrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
